f3a072e5ae729341ce2e9f670dc8aa156fea0e7c
Moises <mhdezcoronado@gmail.com>
2022-05-27 01:54:39 +0200

    Add content mobile

---
+++ features/contentMobile.js
@@ -0,0 +1,32 @@
+document.write(`
+<div class="paragraph" id="contentMobile">
+    <img
+      class="logo"
+      src="./assets/mrgreen-logo-white.svg"
+      alt="mr green logo"
+    />
+    <h1 class="header">
+    Vi dubblar din första insättning upp till 1000 SEK
+    </h1>
+
+    <h2 class="subheader">Spela Casino Online hos Mr Green</h2>
+
+    <p class="header_paragraph">
+    +18 | Endast giltigt för nya spelare med första insättning på min.
+    100kr, max. 1000kr | Bonuspengar måste omsättas 35 gånger för att
+    kunna tas ut | Bonus-Regler och Villkor gäller | stodlinjen.se |
+    spelpaus.se | Spela Ansvarsfullt | Giltigt i 60 dagar.
+    </p>
+
+    <h3 class="login">Redan medlem? Logga in</h3>
+    <button class="btn-register">
+      <span>Registrera häri?</span>
+      <img
+        class="button-icon"
+        src="./assets/bankid-logo.png"
+        alt="bankid logo"
+      />
+    </button>
+</div>
+
+`);
