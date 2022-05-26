313e0728584c7cafba2db52370b7d1bdf27361cd
Moises <mhdezcoronado@gmail.com>
2022-05-27 01:47:12 +0200

    Add footer destop

--- features/desktopFooter.js
+++ features/desktopFooter.js
@@ -1,26 +1,28 @@
 document.write(`
-  <div class="footer" id="footerMobile">
+  <div class="footer" id="footerDesktop">
   <div class="steps step-1">
     <img class="img-steps" src="./assets/step1.svg" alt="mr green logo" />
     <div class="content-steps">
-      <h3 class="step-title">REGISTRERA</h3>
-      <p>Registrera dig enkelt</p>
+      <h3 class="step-title">Click 'Join Now'</h3>
+      <p>Choose online casino bonus</p>
     </div>
   </div>
+
 
   <div class="steps step-2">
     <img class="img-steps" src="./assets/step2.svg" alt="mr green logo" />
     <div class="content-steps">
-      <h3 class="step-title">VÄLKOMSTBONUS CASINO</h3>
-      <p>Välj Casinobonus med din första insättning</p>
+      <h3 class="step-title">Get 100% Deposit Bonus</h3>
+      <p>Plus 200 free spins</p>
     </div>
   </div>
+
 
   <div class="steps step-3">
     <img class="img-steps" src="./assets/step3.svg" alt="mr green logo" />
     <div class="content-steps">
-      <h3 class="step-title">100% BONUS UPP TILL 1000 SEK</h3>
-      <p>Dubbla dina pengar och spela dina favoritspel</p>
+      <h3 class="step-title">Play</h3>
+      <p>In Mr Green's Casino</p>
     </div>
   </div>
 </div>
