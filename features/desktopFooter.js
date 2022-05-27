0befc63219164010075b3d116e1881e658157fe4
Moises <mhdezcoronado@gmail.com>
2022-05-27 12:59:06 +0200

    Add font family

--- features/desktopFooter.js
+++ features/desktopFooter.js
@@ -1,29 +1,29 @@
 document.write(`
   <div class="footer openRegister" id="footerDesktop">
-  <div class="steps step-1 registerButton regitering">
-    <img class="img-steps" src="./assets/step1.svg" alt="mr green logo" />
-    <div class="content-steps">
-      <h3 class="step-title">Click 'Join Now'</h3>
-      <p>Choose online casino bonus</p>
+    <div class="steps step-1 registerButton regitering">
+      <img class="img-steps" src="./assets/step1.svg" alt="mr green logo" />
+      <div class="content-steps">
+        <h3 class="step-title title-step">Click 'Join Now'</h3>
+        <p class="subtitle-step">Choose online casino bonus</p>
+      </div>
+    </div>
+
+
+    <div class="steps step-2">
+      <img class="img-steps" src="./assets/step2.svg" alt="mr green logo" />
+      <div class="content-steps">
+        <h3 class="step-title title-step">Get 100% Deposit Bonus</h3>
+        <p class="subtitle-step">Plus 200 free spins</p>
+      </div>
+    </div>
+
+
+    <div class="steps step-3">
+      <img class="img-steps" src="./assets/step3.svg" alt="mr green logo" />
+      <div class="content-steps">
+        <h3 class="step-title title-step">Play</h3>
+        <p class="subtitle-step">In Mr Green's Casino</p>
+      </div>
     </div>
   </div>
-
-
-  <div class="steps step-2">
-    <img class="img-steps" src="./assets/step2.svg" alt="mr green logo" />
-    <div class="content-steps">
-      <h3 class="step-title">Get 100% Deposit Bonus</h3>
-      <p>Plus 200 free spins</p>
-    </div>
-  </div>
-
-
-  <div class="steps step-3">
-    <img class="img-steps" src="./assets/step3.svg" alt="mr green logo" />
-    <div class="content-steps">
-      <h3 class="step-title">Play</h3>
-      <p>In Mr Green's Casino</p>
-    </div>
-  </div>
-</div>
 `);
--- features/mobileFooter.js
+++ features/mobileFooter.js
@@ -1,27 +1,27 @@
 document.write(`
   <div class="footer" id="footerMobile">
-  <div class="steps step-1 openRegister">
-    <img class="img-steps" src="./assets/step1.svg" alt="mr green logo" />
-    <div class="content-steps">
-      <h3 class="step-title">REGISTRERA</h3>
-      <p>Registrera dig enkelt</p>
+    <div class="steps step-1 openRegister">
+      <img class="img-steps" src="./assets/step1.svg" alt="mr green logo" />
+      <div class="content-steps">
+        <h3 class="step-title title-step">REGISTRERA</h3>
+        <p class="subtitle-step">Registrera dig enkelt</p>
+      </div>
     </div>
-  </div>
 
-  <div class="steps step-2">
-    <img class="img-steps" src="./assets/step2.svg" alt="mr green logo" />
-    <div class="content-steps">
-      <h3 class="step-title">VÄLKOMSTBONUS CASINO</h3>
-      <p>Välj Casinobonus med din första insättning</p>
+    <div class="steps step-2">
+      <img class="img-steps" src="./assets/step2.svg" alt="mr green logo" />
+      <div class="content-steps">
+        <h3 class="step-title title-step">VÄLKOMSTBONUS CASINO</h3>
+        <p class="subtitle-step">Välj Casinobonus med din första insättning</p>
+      </div>
     </div>
-  </div>
 
-  <div class="steps step-3">
-    <img class="img-steps" src="./assets/step3.svg" alt="mr green logo" />
-    <div class="content-steps">
-      <h3 class="step-title">100% BONUS UPP TILL 1000 SEK</h3>
-      <p>Dubbla dina pengar och spela dina favoritspel</p>
+    <div class="steps step-3">
+      <img class="img-steps" src="./assets/step3.svg" alt="mr green logo" />
+      <div class="content-steps">
+        <h3 class="step-title title-step">100% BONUS UPP TILL 1000 SEK</h3>
+        <p class="subtitle-step">Dubbla dina pengar och spela dina favoritspel</p>
+      </div>
     </div>
   </div>
-</div>
 `);
