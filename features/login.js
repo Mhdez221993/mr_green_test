319d992aca5d63a60301d62a0fceef8577737e4a
Moises <mhdezcoronado@gmail.com>
2022-05-27 11:08:54 +0200

    Add login component

---
+++ features/login.js
@@ -0,0 +1,14 @@
+document.write(`
+  <div class="loginModal">
+    <div class="closeLoginBar">
+      <span class="closeLogin">x</span>
+    </div>
+
+    <h2 class="loginTitle">Iniciar Session</h2>
+    <form class="form">
+      <input type="text" placeholder="Email" required />
+      <input type="password" placeholder="Contraseña" required />
+      <button class="submit" type="submit">Iniciar Session</button>
+    </form>
+  </div>
+`)
