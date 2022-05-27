a4f500bb9cdc5bd04cb168dff91fc5e3de6b1a5d
Moises <mhdezcoronado@gmail.com>
2022-05-27 13:35:25 +0200

    Style registration component

--- features/resgisterMobile.js
+++ features/resgisterMobile.js
@@ -1,11 +1,11 @@
 document.write(`
-  <div class="register">
-    <div class="close closeRegister closeRegister">x</div>
+  <div class="register style-registration">
+    <div class="close closeRegister closeRegister">&#x2715;</div>
     <h2 class="registerWelcome">
       Bienvevido a <br />
       Mr Green
     </h2>
-    <h3 class="registerSteps resgister-step-1">
+    <h3 class="registerSteps resgister-step-1 registration-first">
       Empieza por tu correo <br />
       <span>Registrarte te lleva menos de un minuto</span>
     </h3>
