33b02ac4d7368637470891e4422576c7b585ef05
Moises <mhdezcoronado@gmail.com>
2022-05-27 12:29:07 +0200

    Style content

--- features/contentDesktop.js
+++ features/contentDesktop.js
@@ -5,18 +5,18 @@ document.write(`
       src="./assets/mrgreen-logo-white.svg"
       alt="mr green logo"
     />
-    <h1 class="header">
+    <h1 class="header content-title">
       Up to € 100 in 5 Free Bets with the
       20% Bonus on your first deposit
     </h1>
 
-    <h2 class="subheader">
+    <h2 class="subheader content-subtitle">
       100% Profit Boost up to £5000 on
       your first game.
     </h2>
 
-    <button class="btn-register openRegister">
-      <span>Join Now</span>
+    <button class="btn-register openRegister content-register">
+      <span class="join-now">Join Now</span>
       <img
         class="button-icon"
         src="./assets/bankid-logo.png"
@@ -24,7 +24,7 @@ document.write(`
       />
     </button>
 
-    <p class="header_paragraph">
+    <p class="header_paragraph content-las-paragraph">
       Only new players | 18+ | 35x sales requirements
       Terms & Conditions Here | Already a member? <span class="openLogin">Sign in</span>
     </p>
--- features/contentMobile.js
+++ features/contentMobile.js
@@ -5,21 +5,21 @@ document.write(`
       src="./assets/mrgreen-logo-white.svg"
       alt="mr green logo"
     />
-    <h1 class="header">
+    <h1 class="header content-title">
       Vi dubblar din första insättning upp till 1000 SEK
     </h1>
 
-    <h2 class="subheader">Spela Casino Online hos Mr Green</h2>
+    <h2 class="subheader content-subtitle">Spela Casino Online hos Mr Green</h2>
 
-    <p class="header_paragraph">
+    <p class="header_paragraph content-paragraph">
       +18 | Endast giltigt för nya spelare med första insättning på min.
       100kr, max. 1000kr | Bonuspengar måste omsättas 35 gånger för att
       kunna tas ut | Bonus-Regler och Villkor gäller | stodlinjen.se |
       spelpaus.se | Spela Ansvarsfullt | Giltigt i 60 dagar.
     </p>
 
-    <h3 class="login">Redan medlem? <span class="openLogin">Logga in</span></h3>
-    <button class="btn-register openRegister">
+    <h3 class="login content-login">Redan medlem? <span class="openLogin">Logga in</span></h3>
+    <button class="btn-register openRegister content-register">
       <span>Registrera häri?</span>
       <img
         class="button-icon"
